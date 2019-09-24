0a4865c75aa5f1ef1026f17f606401cc15d830c2
wangyikai <1263928214@qq.com>
2019-09-24 13:38:03 +0800

    springboot启动初始化数据库连接池

---
+++ src/main/java/com/example/hikaricpdemo/HikaricpdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.hikaricpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikaricpdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikaricpdemoApplication.class, args);
+    }
+
+}
